7a3cbc7595a54fdb8a9ead7f0eedb4bcd57a46fe
SHEORAN022 <shorinku22@gmail.com>
2025-10-17 11:26:04 +0530

    Resolved merge conflict in Feedback.js

--- models/Feedback.js
+++ models/Feedback.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-// const mongoose = require("mongoose");
-
-// const feedbackSchema = new mongoose.Schema({
-//   name: { type: String, required: true },
-//   email: { type: String, required: true },
-//   message: { type: String, required: true },
-//   rating: { type: Number, min: 1, max: 5 },
-// }, { timestamps: true });
-
-// module.exports = mongoose.model("Feedback", feedbackSchema);
-const mongoose = require("mongoose");
-
-const feedbackSchema = new mongoose.Schema({
-  name: { type:String, required:true },
-  email: { type:String, required:true },
-  message: { type:String, required:true },
-  rating: { type:Number, min:1, max:5 }
-}, { timestamps:true });
-=======
-
-// const mongoose = require("mongoose");
-
-// const feedbackSchema = new mongoose.Schema({
-//   name: { type:String, required:true },
-//   email: { type:String, required:true },
-//   message: { type:String, required:true },
-//   rating: { type:Number, min:1, max:5 }
-// }, { timestamps:true });
-
-// module.exports = mongoose.model("Feedback", feedbackSchema);
-
 const mongoose = require("mongoose");
 
 const feedbackSchema = new mongoose.Schema(
@@ -41,6 +9,5 @@ const feedbackSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
->>>>>>> efd1034d73f8736393e914cd597bd707ab3ed49b
 
 module.exports = mongoose.model("Feedback", feedbackSchema);
